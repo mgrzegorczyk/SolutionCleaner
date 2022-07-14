2bc225826e42b4f600c8465cfbd41f3b186dd6fe
mgrzegor <mgrzegor@protonmail.com>
2022-07-14 19:39:08 +0200

    added ProjectCleaner tests

---
+++ tests/SolutionCleaner.Tests/ProjectCleanerTests.cs
@@ -0,0 +1,43 @@
+﻿using SolutionCleaner.Services;
+using System;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace SolutionCleaner.Tests
+{
+    public class ProjectCleanerTests
+    {
+        private readonly ProjectCleaner projectCleaner;
+        private readonly MockFileSystem fileSystem;
+        private readonly string projectDirPath = Path.Combine(@"c:\");
+
+        public ProjectCleanerTests()
+        {
+            this.fileSystem = new MockFileSystem();
+            this.projectCleaner = new ProjectCleaner(fileSystem);
+        }
+
+        [Fact]
+        public void RemoveVsDirectory_ShouldRemoveVsDirectory_WhenPathIsCorrect()
+        {
+            //Arrange
+            var vsFolderFullPath = Path.Combine(projectDirPath, ".vs");
+
+            fileSystem.AddDirectory(".vs");
+
+            //Act
+            projectCleaner.RemoveVsDirectory(projectDirPath);
+
+            //Assert
+            Assert.False(fileSystem.Directory.Exists(vsFolderFullPath));
+        }
+
+        [Fact]
+        public void RemoveVsDirectory_ShouldThrowArgumentNullException_WhenProjectDirPathIsNull()
+        {
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => projectCleaner.RemoveVsDirectory(null));
+        }
+    }
+}
